e41413877b25c5b46cf198104be6941f3154b97d
afranko <ernest.franko@gmail.com>
2018-07-02 13:41:51 +0200

    Initial commit

---
+++ main/main.c
@@ -0,0 +1,19 @@
+/* main.c - Only for tasks!
+  Call your own task in the main loop and/or call
+  an initilazion function before the main loop if it's needed.
+  */
+
+/* Includes */
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
+void app_main()
+{
+    //INIT
+
+    for(;;)
+    {
+        //TASKS
+    }
+}
---
+++ makefile
@@ -0,0 +1,10 @@
+# AITIA VAHE project makefile
+
+# DO NOT MODIFY THIS FILE! - PLS PLS PLS
+
+PROJECT_NAME := vahe
+
+# Uncomment this line below if any external lib is added to the project!
+# COMPONENT_ADD_INCLUDEDIRS := components/include
+
+include $(IDF_PATH)/make/project.mk

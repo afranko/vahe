5e135b5f20e0befb689fd36c88438b62773c8603
afranko <ernest.franko@gmail.com>
2018-07-02 13:54:15 +0200

    Initial Commit v2 (add includes)

--- main/main.c
+++ main/main.c
@@ -1,12 +1,13 @@
 /* main.c - Only for tasks!
   Call your own task in the main loop and/or call
-  an initilazion function before the main loop if it's needed.
+  an initialization function before the main loop if it's needed.
   */
 
 /* Includes */
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
+#include "esp_system.h"
 
 void app_main()
 {
